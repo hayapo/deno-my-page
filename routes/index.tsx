a67a3cae83ba946227d6d8c64c9de4722b0a8729
hayapo <keep.forward1001@gmail.com>
2023-07-26 07:09:57 +0900

    [feature] Aboutページの実装

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,8 @@
 import { Head } from "$fresh/runtime.ts";
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { microcmsClient } from "../components/microcmsClient.ts";
 import { BioType } from "../types/microcmsResponseType.ts";
+import { calcAge, microcmsClient, parseStringToArray } from "../lib/index.ts";
+import { NavBar, SocialIcons } from "../components/index.ts";
 
 export const handler: Handlers<BioType | null> = {
   async GET(_, ctx) {
@@ -15,33 +14,53 @@ export const handler: Handlers<BioType | null> = {
   },
 };
 
-export default function Home({ data }: PageProps<BioType | null>) {
+export default function About({ data }: PageProps<BioType | null>) {
   if (!data) {
     return <h1>Bio data not found</h1>;
   }
+
+  const languageList = parseStringToArray(data.favoriteLanguage);
+  const hobbyList = parseStringToArray(data.hobby);
+
   console.log(data);
-  const count = useSignal(3);
+
   return (
     <>
       <Head>
         <title>{data.handleName}'s Page</title>
       </Head>
-      <div class="px-4 py-8 mx-auto">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">Welcome to fresh</h1>
-          <p class="my-4">
-            Try updating this message in the
-            <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-          </p>
-          <p>{data.currentStatus}</p>
-          <Counter count={count} />
+      <NavBar isAbout={true} />
+      <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center font-mono text-center">
+        <img
+          class="mt-3 border-2 rounded-[30%] border-gray-400"
+          src="/avator_icon.svg"
+          width="250"
+          height="250"
+          alt="my avator icon: a pink hair girl with a black cloth"
+        />
+        <SocialIcons socials={data.socials} class="my-5" />
+        <div class="flex flex-col gap-7 text-2xl">
+          <div class="flex flex-col gap-2">
+            <p class="text-5xl font-bold">{data.handleName}</p>
+            <p>
+              {data.lastNameEn + " " + data.firstNameEn}
+            </p>
+          </div>
+          <div class="flex flex-col gap-2 text-2xl">
+            <p>age: {calcAge(data.dateOfBirth)}</p>
+            <p>{data.currentJob}やってます</p>
+            <p>@{data.whereLiving}</p>
+          </div>
+          <div class="flex flex-col gap-2 text-2xl">
+            <p>
+              好きな言語: <br />
+              {languageList.join(" / ")}
+            </p>
+            <p>
+              趣味 <br />
+              {hobbyList.join(" / ")}
+            </p>
+          </div>
         </div>
       </div>
     </>

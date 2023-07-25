7dff9764cc226b3917e6a46bd1c972974b5901a2
hayapo <keep.forward1001@gmail.com>
2023-07-26 07:07:52 +0900

    [feature] careerページの作成

---
+++ routes/career/index.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../../components/NavBar.tsx";
+
+export default function Career() {
+  return (
+    <>
+      <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center font-mono text-center">
+        <NavBar isAbout={false} />
+        <div class="text-4xl">Career Page (In Development)</div>
+      </div>
+    </>
+  );
+}

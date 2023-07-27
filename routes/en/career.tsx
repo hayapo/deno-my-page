ca9c9ce3783b20bcb7a4d74c7808d734e107ab60
hayapo <keep.forward1001@gmail.com>
2023-07-28 07:26:53 +0900

    [feature] 英語ページを作った (WIP)

---
+++ routes/en/career.tsx
@@ -0,0 +1,32 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { ProfileType } from "../../types/microcmsResponseType.ts";
+import { microcmsClient } from "../../lib/index.ts";
+
+const ENDPOINT = "profile_en";
+const cache: Record<string, ProfileType> = {};
+
+export const handler: Handlers<ProfileType | null> = {
+  async GET(_, ctx) {
+    if (cache[ENDPOINT]) {
+      console.log("returned from cache");
+      return ctx.render(cache[ENDPOINT]);
+    }
+
+    const bioData = await microcmsClient.get({
+      endpoint: ENDPOINT,
+      queries: { limit: 99 },
+    });
+    cache[ENDPOINT] = bioData;
+    console.log(`api called`);
+    return ctx.render(bioData);
+  },
+};
+export default function Career({ data }: PageProps<ProfileType | null>) {
+  return (
+    <>
+      <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center font-mono text-center">
+        <div class="text-4xl">English Career Page (In Development)</div>
+      </div>
+    </>
+  );
+}
--- routes/en/index.tsx
+++ routes/en/index.tsx
@@ -1,68 +1,114 @@
 import { Head } from "$fresh/runtime.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { BioType } from "../../types/microcmsResponseType.ts";
+import { ProfileType } from "../../types/microcmsResponseType.ts";
 import {
   calcAge,
   microcmsClient,
   parseStringToArray,
 } from "../../lib/index.ts";
+import { SocialIcons } from "../../components/index.ts";
 
-export const handler: Handlers<BioType | null> = {
+const ENDPOINT = "profile_en";
+const cache: Record<string, ProfileType> = {};
+
+export const handler: Handlers<ProfileType | null> = {
   async GET(_, ctx) {
+    if (cache[ENDPOINT]) {
+      console.log("returned from cache");
+      return ctx.render(cache[ENDPOINT]);
+    }
+
     const bioData = await microcmsClient.get({
-      endpoint: "bio_jp",
+      endpoint: ENDPOINT,
       queries: { limit: 99 },
     });
+    cache[ENDPOINT] = bioData;
+    console.log(`api called`);
     return ctx.render(bioData);
   },
 };
 
-export default function Home({ data }: PageProps<BioType | null>) {
+export default function About({ url, data }: PageProps<ProfileType | null>) {
   if (!data) {
     return <h1>Bio data not found</h1>;
   }
   const languageList = parseStringToArray(data.favoriteLanguage);
   const hobbyList = parseStringToArray(data.hobby);
-
-  console.log(data);
   return (
     <>
       <Head>
         <title>{data.handleName}'s Page</title>
       </Head>
-      <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center font-mono text-center">
-        <img
-          class="my-6 border-2 rounded-[30%] border-gray-400"
-          src="/avator_icon.svg"
-          width="300"
-          height="300"
-          alt="my avator icon: a pink hair girl with a black cloth"
-        >
-        </img>
-        <div class="flex flex-col gap-2">
-          <p class="text-5xl font-bold">{data.handleName}</p>
-          <p class="text-2xl">
-            {data.lastNameEn + " " + data.firstNameEn}
-          </p>
-        </div>
-        <div class="mt-4 flex flex-col gap-10 text-2xl">
-          <div class="flex flex-col gap-2">
-            <p>age: {calcAge(data.dateOfBirth)}</p>
-            <p>{data.currentJob}やってます</p>
-            <p>@{data.whereLiving}</p>
-          </div>
-          <div class="mt-3 flex flex-col gap-3">
-            <p>
-              好きな言語: <br />
-              {languageList.join(" / ")}
-            </p>
-            <p>
-              趣味 <br />
-              {hobbyList.join(" / ")}
-            </p>
-          </div>
+      {/* <NavBar isAbout /> */}
+      <div class="max-w-screen-lg mx-auto flex flex-col gap-7 items-center justify-center font-mono text-center">
+        <Avator />
+        <SocialIcons socials={data.socials} />
+        <div class="flex flex-col gap-10">
+          <Name
+            handleName={data.handleName}
+            lastNameEn={data.lastNameEn}
+            firstNameEn={data.firstNameEn}
+          />
+          <Bio
+            dateOfBirth={data.dateOfBirth}
+            currentJob={data.currentJob}
+            whereLiving={data.whereLiving}
+          />
+          <Favorites languageList={languageList} hobbyList={hobbyList} />
         </div>
       </div>
     </>
+  );
+}
+
+function Avator() {
+  return (
+    <img
+      class="mt-3 border-2 rounded-[100%] border-gray-400"
+      src="/avator_icon.svg"
+      width="250"
+      height="250"
+      alt="my avator icon: a pink hair girl with a black cloth"
+    />
+  );
+}
+
+function Name(
+  props: { handleName: string; lastNameEn: string; firstNameEn: string },
+) {
+  return (
+    <div class="flex flex-col gap-2">
+      <p class="text-5xl font-bold">{props.handleName}</p>
+      <p class="text-2xl">
+        {props.lastNameEn + " " + props.firstNameEn}
+      </p>
+    </div>
+  );
+}
+
+function Bio(
+  props: { dateOfBirth: string; currentJob: string; whereLiving: string },
+) {
+  return (
+    <div class="flex flex-col gap-2 text-2xl">
+      <p>age: {calcAge(props.dateOfBirth)}</p>
+      <p>{props.currentJob}やってます</p>
+      <p>@{props.whereLiving}</p>
+    </div>
+  );
+}
+
+function Favorites(props: { languageList: string[]; hobbyList: string[] }) {
+  return (
+    <div class="flex flex-col gap-5 text-2xl">
+      <p>
+        Favorite languages: <br />
+        {props.languageList.join(" / ")}
+      </p>
+      <p>
+        Hobbies: <br />
+        {props.hobbyList.join(" / ")}
+      </p>
+    </div>
   );
 }

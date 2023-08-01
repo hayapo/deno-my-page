d25dfcf35e7aed0644138c4a8fceeed20f271296
hayapo <keep.forward1001@gmail.com>
2023-08-02 03:17:22 +0900

    [feature] Headコンポーネントの作成

---
+++ components/Head.tsx
@@ -0,0 +1,19 @@
+import { Head } from "$fresh/runtime.ts";
+
+type CustonHeadProps = {
+  title?: string;
+  description?: string;
+};
+
+export function CustomHead({ title, description }: CustonHeadProps) {
+  const siteTitle = title || "hayapo's website";
+  const siteDescription = description || "website of hayapo profile";
+  return (
+    <Head>
+      <title>{`${siteTitle} | hayapo`}</title>
+      <meta name="description" content={description}></meta>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta charSet="utf-8" />
+    </Head>
+  );
+}
